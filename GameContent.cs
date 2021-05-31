afe421093a6ac748ea8537ca98a5c1c915a378a5
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2021-05-31 21:47:00 +0200

    Update GameContent.cs

--- GameContent.cs
+++ GameContent.cs
@@ -12,19 +12,16 @@ namespace Breakout
 {
     class GameContent
     {
-        public Texture2D imgBrick { get; set; }
+    
         public Texture2D imgPaddle { get; set; }
-        public Texture2D imgBall { get; set; }
-        public Texture2D imgPixel { get; set; }
-        public SpriteFont labelFont { get; set; }
+     
 
         public GameContent(ContentManager Content)
         {
             
-            imgBall = Content.Load<Texture2D>("Ball");
-            imgPixel = Content.Load<Texture2D>("Pixel");
+        
             imgPaddle = Content.Load<Texture2D>("Paddle");
-            imgBrick = Content.Load<Texture2D>("Brick");
+          
 
         }
     }
